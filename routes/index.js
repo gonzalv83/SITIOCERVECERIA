6a74bd5d0c4d4578795229818be1ca2141afa57f
Gonzalo Lopez Varela <gonza.lvarela@gmail.com>
2022-06-21 13:17:22 -0300

    tarea formulario de contacto

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'gonza.lvarela@gmail.com',
+    subject: 'Contacto desde la Web',
+    html: nombre + " " + apellido + " se contacto a traves del sitio y quiere mas info a este correo: " + email + ". <br> Ademas, hizo el siguiente comentario: " + mensaje + ". <br> Su tel es " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+  
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+  
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  })
+
+})
+
+
+
 
 module.exports = router;
