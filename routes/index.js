3ce41cc8c971b6fcca613d01d17347f45b2ff19f
Gonzalo Lopez Varela <gonza.lvarela@gmail.com>
2022-08-18 12:41:54 -0300

    tarea unidad 3 modulo 5

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,31 @@ const async = require('hbs/lib/async');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
-
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   novedades = await novedadesModel.getNovedades();
   novedades = novedades.splice(0,5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg'
+      }
+    }
+  });
+
+
   res.render('index',{
     novedades
   });
